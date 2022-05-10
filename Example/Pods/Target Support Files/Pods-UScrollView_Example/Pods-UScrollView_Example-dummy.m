7776b01ecfd3371eb31ac81475674ee7545cc31d
devudaya <niroshan127@gmail.com>
2022-05-10 19:14:28 +0530

    initial commit with cocoapod lib

---
+++ Example/Pods/Target Support Files/Pods-UScrollView_Example/Pods-UScrollView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UScrollView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_UScrollView_Example
+@end
---
+++ Example/Pods/Target Support Files/UScrollView/UScrollView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UScrollView : NSObject
+@end
+@implementation PodsDummy_UScrollView
+@end
